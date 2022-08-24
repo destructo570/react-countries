b2f951dfa8f30ab45312896638ff5f42093855b9
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-08-24 15:10:39 +0530

    Finish the filtering logic

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -27,3 +27,16 @@ export const setUserThemePref = (value) => {
   localStorage.setItem("theme", value);
   return value;
 };
+
+export const debounce = (fn, delay) => {
+  let timer;
+  return function () {
+    let context = this,
+      args = arguments;
+
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, delay);
+  };
+};
--- src/pages/CountriesPage.js
+++ src/pages/CountriesPage.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useMemo } from "react";
+import React, { useState, useEffect, useMemo, useCallback } from "react";
 import Countries from "../components/countries/Countries";
 import DropDown from "../components/form/dropDown/DropDown";
 import Search from "../components/search/Search";
 import countriesData from "../fixture/countryData.json";
-
-const DummyFilter = ["None", "Africa", "America", "Asia", "Europe", "Oceania"];
+import { debounce } from "../helpers/helper";
+const DummyFilter = ["None", "Africa", "Americas", "Asia", "Europe", "Oceania"];
 export default function CountriesPage() {
   const [countries, setCountries] = useState(countriesData);
   const [filteredCountries, setFilteredCountries] = useState(countries);
@@ -12,27 +12,62 @@ export default function CountriesPage() {
   const [filterInput, setFilterInput] = useState("");
 
   useEffect(() => {
-    // console.log("1");
-    // console.log(filterInput);
-    // let result = "";
-    // if (!searchInput && !filterInput) {
-    //   result = countries;
-    // } else {
-    //   result = countries.filter((item) => {
-    //     const countryName = item.name.common.toLowerCase();
-    //     const regionName = item.region.toLowerCase();
-    //     const inputValue = searchInput.toLowerCase();
-    //     const filterValue = filterInput.toLowerCase();
-    //     if (!searchInput) {
-    //       return regionName === filterValue;
-    //     } else {
-    //       console.log("here");
-    //       return countryName.indexOf(inputValue);
-    //     }
-    //   });
-    // }
-    // setFilteredCountries(result);
+    let result = "";
+    if (!searchInput && !filterInput) {
+      result = countries;
+    } else {
+      result = countries.filter((item) => {
+        const countryName = item.name.common.toLowerCase();
+        const regionName = item.region.toLowerCase();
+        const inputValue = searchInput.toLowerCase();
+        const filterValue = filterInput.toLowerCase();
+        console.log("Runnin...");
+        if (!searchInput) {
+          return regionName === filterValue;
+        } else if (!filterInput || filterValue === "none") {
+          return countryName.indexOf(inputValue) === 0;
+        } else {
+          return (
+            regionName === filterValue && countryName.indexOf(inputValue) === 0
+          );
+        }
+      });
+    }
+    setFilteredCountries(result);
   }, [searchInput, filterInput, countries]);
+
+  // const filterHandler = () => {
+  //   let result = "";
+  //   if (!searchInput && !filterInput) {
+  //     result = countries;
+  //   } else {
+  //     result = countries.filter((item) => {
+  //       const countryName = item.name.common.toLowerCase();
+  //       const regionName = item.region.toLowerCase();
+  //       const inputValue = searchInput.toLowerCase();
+  //       const filterValue = filterInput.toLowerCase();
+  //       console.log("Runnin...");
+  //       if (!searchInput) {
+  //         return regionName === filterValue;
+  //       } else if (!filterInput || filterValue === "none") {
+  //         return countryName.indexOf(inputValue) === 0;
+  //       } else {
+  //         return (
+  //           regionName === filterValue && countryName.indexOf(inputValue) === 0
+  //         );
+  //       }
+  //     });
+  //   }
+  //   setFilteredCountries(result);
+  // };
+  // const debouncedFilterHandler = useCallback(debounce(filterHandler, 1000), []);
+
+  // useEffect(debouncedFilterHandler, [searchInput, filterInput, countries]);
+
+  // const debouncedFilterHandler = useMemo(
+  //   useCallback(debounce(filterHandler, 1000), []),
+  //   [searchInput, filterInput, countries]
+  // );
 
   const onInputChange = (e) => {
     setSearchInput(e.target.value);
